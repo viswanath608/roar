37622a81087b8fd771b09c4840be77b6d16c9002
rwarasaurus <info@kieronwilson.co.uk>
2012-08-17 16:38:14 +0100

    started admin sections

---
+++ roar/routes/admin/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+	Main welcome page
+*/
+Route::get('admin/dashboard', function() {
+	$vars['messages'] = Notify::read();
+	$vars['token'] = Csrf::token();
+	
+	return View::make('dashboard', $vars)
+		->nest('header', 'partials/header')
+		->nest('footer', 'partials/footer');
+});
---
+++ roar/views/dashboard.php
@@ -0,0 +1,13 @@
+<?php echo $header; ?>
+
+			<h1><?php echo __('dashboard.dashboard', 'Dashboard'); ?></h1>
+
+			<?php echo $messages; ?>
+
+			<section class="content">
+
+				Welcome to the dashboard
+
+			</section>
+
+<?php echo $footer; ?>

0bd72afb895cf86b19fb64d8eebd9584afbe983f
rwarasaurus <info@kieronwilson.co.uk>
2012-11-08 16:41:23 +0000

    cleaner way of getting the env

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 */
 
 define('DS', '/');
-define('ENV', (isset($_ENV['APP_ENV']) ? $_ENV['APP_ENV'] : 'production'));
+define('ENV', getenv('APP_ENV'));
 define('VERSION', '0.1');
 
 define('PATH', dirname(__FILE__) . DS);
